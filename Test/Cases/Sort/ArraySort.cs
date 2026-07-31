b33d560cd525cea2ce84493f9f14495a806afa59
Sean <hses106021@gmail.com>
2026-07-31 20:57:46 +0800

    feat: add ArraySort test method

---
+++ Test/Cases/Sort/ArraySort.cs
@@ -0,0 +1,59 @@
+using SeanOne.Alchemy.Test.Cases.Error;
+using System.Collections.Generic;
+
+namespace SeanOne.Alchemy.Test.Test.Cases.Sort
+{
+    public class SortAS_Empty : SortTestBase
+    {
+        public SortAS_Empty() : base(
+            input: new List<int>(),
+            expected: new List<int>(),
+            instruction: "arr /sort:as"
+        ) { }
+    }
+
+    public class SortAS_Ascending : SortTestBase
+    {
+        public SortAS_Ascending() : base(
+            input: new List<int>() { 5, 4, 3, 2, 1 },
+            expected: new List<int>() { 1, 2, 3, 4, 5 },
+            instruction: "arr /sort:as"
+        ) { }
+    }
+
+    public class SortAS_AlreadySorted : SortTestBase
+    {
+        public SortAS_AlreadySorted() : base(
+            input: new List<int>() { 1, 2, 3, 4, 5 },
+            expected: new List<int>() { 1, 2, 3, 4, 5 },
+            instruction: "arr /sort:as"
+        ) { }
+    }
+
+    public class SortAS_Descending : SortTestBase
+    {
+        public SortAS_Descending() : base(
+            input: new List<int>() { 1, 2, 3, 4, 5 },
+            expected: new List<int>() { 5, 4, 3, 2, 1 },
+            instruction: "arr /sort:asd"
+        ) { }
+    }
+
+    public class SortAS_AlreadySortedDesc : SortTestBase
+    {
+        public SortAS_AlreadySortedDesc() : base(
+            input: new List<int>() { 3, 1, 3, 2, 2 },
+            expected: new List<int>() { 5, 4, 3, 2, 1 },
+            instruction: "arr /sort:asd"
+        ) { }
+    }
+
+    public class SortAS_Duplicate : SortTestBase
+    {
+        public SortAS_Duplicate() : base(
+            input: new List<int>() { 3, 1, 3, 2, 2 },
+            expected: new List<int>() { 1, 2, 2, 3, 3 },
+            instruction: "arr /sort:as"
+        ) { }
+    }
+}
---
+++ Test/Cases/Sort/SortTestBase.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace SeanOne.Alchemy.Test.Test.Cases.Sort
+{
+    public abstract class SortTestBase : ITest
+    {
+        private readonly List<int> input;
+        private readonly List<int> expected;
+        private readonly string ins;
+
+        protected SortTestBase(List<int> input, List<int> expected, string instruction)
+        {
+            this.input = input;
+            this.expected = expected;
+            this.ins = instruction;
+        }
+
+        public void Setup() { /* Put extra initialization here; leave blank for now */ }
+
+        public string Run() =>
+            string.Join(string.Empty, Alchemy.Transform(input, ins).GetInt32List());
+
+        public string GetAnswer() => string.Join(string.Empty, expected);
+    }
+}
